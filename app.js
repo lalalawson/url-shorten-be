731ea31622c40a0cea65ef68eb64066654ce48c5
lalalawson <lawson.tkw@gmail.com>
2022-09-13 18:46:19 +0800

    added urlschema to save data in the right format

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const httpServer = require("http").createServer(app);
 app.use(cors());
 const db = require("./db/db.config");
 
+// Test endpoint to ensure server is up.
 app.get("/", function (req, res) {
   res.send("Welcome to USS!");
 });
+
+// Model for the url object
+const Url = require("./models/urlModel");
 
 httpServer.listen(port, () => {
   console.log(`USS backend is running on port ${port}...`);
